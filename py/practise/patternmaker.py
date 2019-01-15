23c679d88b715b634496c609ca11c3605304d13a
cragwen <cragwen@gmail.com>
2019-01-15 16:20:20 +0800

    add

--- py/practise/patternmaker.py
+++ py/practise/patternmaker.py
@@ -1,12 +1,11 @@
 import re
 
-Pattern = """\
-#define TCPWM_1_INTERRUPTS_{0}_HANDLER                  Ocu_Isr_Vector_{1}_Cat2              // IRQ{1}"""
+Pattern = """#define TCPWM_0_INTERRUPTS_{0}_HANDLER                     Ocu_Isr_Vector_{1}_Cat2              // IRQ{1}"""
 
-Times   = 13
+Times   = 12
 
-Loop0   = 512
-Loop1   = 537
+Loop0   = 256
+Loop1   = 363
 Loop2   = Loop0 + 1
 
 for x in range(Times):
