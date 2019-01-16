3c2326a7608cf426b57549287d47bdab0eaefee8
cragwen <cragwen@gmail.com>
2019-01-16 12:47:29 +0800

    update

--- py/practise/patternmaker.py
+++ py/practise/patternmaker.py
@@ -1,15 +1,15 @@
 import re
 
-Pattern = """#define TCPWM_0_INTERRUPTS_{0}_HANDLER                     Ocu_Isr_Vector_{1}_Cat2              // IRQ{1}"""
+loop = 12
+rep  = [256, 363]
+pat  = """#define TCPWM_0_INTERRUPTS_{0[0]}_HANDLER                     Ocu_Isr_Vector_{0[1]}_Cat2              // IRQ{0[1]}"""
 
-Times   = 12
 
-Loop0   = 256
-Loop1   = 363
-Loop2   = Loop0 + 1
+def patmak(pat, loop, rep):
+    """ this is a pattern maker"""
+    for i in range(loop):
+        print(pat.format(rep))
+        rep = [x+1 for x in rep]
 
-for x in range(Times):
-    print( Pattern.format( str(Loop0), str(Loop1) ) )
-    Loop0 += 1
-    Loop1 += 1
-    Loop2 += 1
+if __name__ == '__main__':
+    patmak( pat, loop, rep)
