26979a1f43ae82d5530f5fb3de22de969e59af86
cragwen <cragwen@gmail.com>
2019-01-25 15:52:32 +0800

    test2

--- py/practise/pat.py
+++ py/practise/pat.py
@@ -1,9 +1,15 @@
-import PatternMaker
+from PatternMaker import *
 
 a = Pattern()
-a.loop = 20
-a.rep  = [1, 100, 500]
-a.pat  = """## {1}   
-   1.{0}      2. {2}  - 例文 {0} 
----"""
+a.loop = 4
+a.rep  = [512, 349]
+a.pat  = """  {{
+    {{
+        .sysIntSrc  = tcpwm_0_interrupts_{0}_IRQn,
+        .intIdx     = CPUIntIdx0_IRQn,
+        .isEnabled  = true,
+    }},
+    Ocu_Isr_Vector_{1}_Cat2,
+    3,
+  }},"""
 a.patmak()
